90c5148aac4ec91098dac9292c1df791bfcfed31
Nadir Izrael <nadir.izr@gmail.com>
2016-03-09 14:54:46 +0200

    Added support for external volumes, and added bazel required volumes for sources and output.

--- dazel.py
+++ dazel.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import types
 
 
 DAZEL_RC_FILE = ".dazelrc"
@@ -13,6 +14,8 @@ DEFAULT_LOCAL_DOCKERFILE = "Dockerfile.dazel"
 DEFAULT_REMOTE_RPOSITORY = "dazel"
 DEFAULT_DIRECTORY = os.getcwd()
 DEFAULT_COMMAND = "/bazel/output/bazel"
+DEFAULT_VOLUMES = []
+DEFAULT_BAZEL_USER_OUTPUT_ROOT = "%s/.cache/bazel" % os.environ.get("HOME", "~")
 
 
 class DockerInstance:
@@ -24,14 +27,18 @@ class DockerInstance:
     """
     
     def __init__(self, instance_name, image_name, dockerfile, repository,
-                       directory, command, dazel_run_file):
+                       directory, command, volumes, bazel_user_output_root,
+                       dazel_run_file):
         self.instance_name = instance_name
         self.image_name = image_name
         self.dockerfile = dockerfile
         self.repository = repository
         self.directory = directory
         self.command = command
+        self.bazel_user_output_root = bazel_user_output_root
         self.dazel_run_file = dazel_run_file
+
+        self._add_volumes(volumes)
         
     @classmethod
     def from_config(cls):
@@ -44,11 +51,15 @@ class DockerInstance:
                 repository=config.get("DAZEL_REPOSITORY", DEFAULT_REMOTE_RPOSITORY),
                 directory=config.get("DAZEL_DIRECTORY", DEFAULT_DIRECTORY),
                 command=config.get("DAZEL_COMMAND", DEFAULT_COMMAND),
+                volumes=config.get("DAZEL_VOLUMES", DEFAULT_VOLUMES),
+                bazel_user_output_root=config.get("DAZEL_BAZEL_USER_OUTPUT_ROOT",
+                                                  DEFAULT_BAZEL_USER_OUTPUT_ROOT),
                 dazel_run_file=config.get("DAZEL_RUN_FILE", DAZEL_RUN_FILE))
 
     def send_command(self, args):
-        command = "docker exec -it %s %s %s" % (
-            self.instance_name, self.command, '"%s"' % '" "'.join(args))
+        command = "docker exec -it %s %s --output_user_root=%s %s" % (
+            self.instance_name, self.command, self.bazel_user_output_root,
+            '"%s"' % '" "'.join(args))
         return os.system(command)
 
     def start(self):
@@ -71,9 +82,9 @@ class DockerInstance:
         print "Starting docker container '%s'..." % self.instance_name
         command = "docker stop %s >& /dev/null ; " % (self.instance_name)
         command += "docker rm %s >& /dev/null ; " % (self.instance_name)
-        command += "docker run -id --name=%s %s/%s /bin/bash " % (
-            self.instance_name, self.repository, self.image_name)
-        #command += '/bin/bash -c "while true; do ping 8.8.8.8; done"'
+        command += "docker run -id --name=%s -w %s %s %s/%s /bin/bash" % (
+            self.instance_name, os.path.realpath(self.directory),
+            self.volumes, self.repository, self.image_name)
         rc = os.system(command)
         if rc:
             return rc
@@ -113,6 +124,38 @@ class DockerInstance:
 
         command = "docker pull %s/%s" % (self.repository, self.image_name)
         return os.system(command)
+
+    def _add_volumes(self, volumes):
+        """Add the given volumes to the run string, and the bazel volumes we need anyway."""
+        # DAZEL_VOLUMES can be a python iterable or a comma-separated string.
+        if isinstance(volumes, str):
+            volumes = [v.strip() for v in volumes.split(",")]
+        elif volumes and not isinstance(volumes, types.Iterable):
+            raise RuntimeError("DAZEL_VOLUMES must be comma-separated string "
+                               "or python iterable of strings")
+
+        # Find the real source and output directories.
+        real_directory = os.path.realpath(self.directory)
+        real_bazelout = os.path.realpath(
+            os.path.join(self.directory, "bazel-out", ".."))
+        volumes += [
+            "%s:%s" % (real_directory, real_directory),
+            "%s:%s" % (real_bazelout, real_bazelout),
+        ]
+        self.volumes = '-v "%s"' % '" -v "'.join(volumes)
+
+        # If the user hasn't explicitly set a DAZEL_BAZEL_USER_OUTPUT_ROOT for
+        # bazel, set it from the output directory so that we get the build
+        # results on the host.
+        if not self.bazel_user_output_root and "/_bazel" in real_bazelout:
+            parts = real_bazelout.split("/_bazel")
+            first_part = parts[0]
+            second_part = "/_bazel" + parts[1].split("/")[0]
+            self.bazel_user_output_root = first_part + second_part
+
+        # Make sure the path exists on the host.
+        if not os.path.isdir(self.bazel_user_output_root):
+            os.makedirs(self.bazel_user_output_root)
 
     @classmethod
     def _config_from_file(cls):
